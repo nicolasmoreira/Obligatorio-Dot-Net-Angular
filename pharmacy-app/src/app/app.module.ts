663228d77093588ed58fd5a5b5d4e867b8d39a6e
Nicolas Moreira <nicolas.moreira@linkuy.com>
2019-11-28 21:13:36 -0300

    Header was added

--- pharmacy-app/src/app/app.module.ts
+++ pharmacy-app/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderModule } from './header/header.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
