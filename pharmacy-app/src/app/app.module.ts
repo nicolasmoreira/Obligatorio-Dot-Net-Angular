b23b3e1bef5cc98e13546e7989081025142d6d2c
Dario <daro1920@gmail.com>
2019-11-30 12:56:02 -0300

    Productos polulares: Lista con mock, falta incluir llamada a la API

--- pharmacy-app/src/app/app.module.ts
+++ pharmacy-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ProductListComponent } from './productList/product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
---
+++ pharmacy-app/src/app/productList/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent {
+
+    products: Product[] =[  
+        {image:'assets/images/product_01.png',name:'Bioderma',price:'95'},  
+        {image:'assets/images/product_02.png',name:'Chanca Piedra',price:'70'},  
+        {image:'assets/images/product_03.png',name:'Umcka Cold Care',price:'120'},  
+        {image:'assets/images/product_04.png',name:'Cetyl Pure',price:'45'},  
+        {image:'assets/images/product_05.png',name:'CLA Core',price:'38'},  
+        {image:'assets/images/product_06.png',name:'Poo Pourri',price:'89'},  
+    ]  
+
+
+}
+
+class Product {  
+  image : string;  
+  name : string;  
+  price : string;    
+}
