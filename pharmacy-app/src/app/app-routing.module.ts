374415e75160ad731b979979b51201332d29557f
Nicolas Moreira <nicolas.moreira@linkuy.com>
2019-12-08 18:11:10 -0300

    Help page was added

--- pharmacy-app/src/app/app-routing.module.ts
+++ pharmacy-app/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './login/login.component';
 import { CartComponent } from './cart/cart.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { HelpComponent } from './help/help.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'help', component: HelpComponent },
   { path: '', component: HomeComponent }
 ];
 
